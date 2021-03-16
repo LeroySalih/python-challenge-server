631e44c0235e8e00e84ae27f5b43fb018e8e7d35
Leroy Salih <Leroy.Salih@gmail.com>
2021-03-16 06:49:39 +0300

    Dev

--- pages/api/submission.js
+++ pages/api/submission.js
@@ -1,7 +1,15 @@
+
+import Db from 'mongodb/lib/db';
+import {connectToDatabase} from '../../utils/mongodb';
+
+
 export default function handler(req, res) {
 
+  const {db} = await connectToDatabase()
   const {data} = req.query
   console.log(data);
+
+  db.collections("subnmissions").insertOne(data);
   
   res.status(200).json({ name: 'Data Received' })
 }
