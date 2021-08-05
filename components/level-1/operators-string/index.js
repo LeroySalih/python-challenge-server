9a932828d1ff62ca0f8298579eb2aa18ecc1fa69
Leroy Salih <leroy.salih@gmail.com>
2021-08-05 13:07:40 +0000

    😍

--- components/level-1/operators-string/index.js
+++ components/level-1/operators-string/index.js
@@ -46,7 +46,9 @@ The middle letter is l
             <KeyTerm index="1" term="String">A collection of 1 or more characters, enclosed. with a quote. 'Salih', 'salih@bisak.org', '12345' are all strings</KeyTerm>        
             <KeyTerm index="2" term="Index">The position of a character in a string (starts at Zero)</KeyTerm>
             <KeyTerm index="3" term="Index">The position of a character in a string (starts at Zero)</KeyTerm>
-            <KeyTerm index="4" term="[]">The index operator.  Returns the character</KeyTerm>        
+            <KeyTerm index="4" term="+">The append operator.  Joins two strings together.</KeyTerm> 
+            <KeyTerm index="5" term="*">The multiply operator.  Repeats characters multiple times</KeyTerm> 
+            <KeyTerm index="6" term="[]">The index operator.  Returns the character specified </KeyTerm>        
             
             
         </KeyTerms>
@@ -54,47 +56,115 @@ The middle letter is l
     <Section>
         <SectionTitle >Declaring Strings</SectionTitle>
         <SectionText>
-            <p>Perform a calculation in python takes 2 steps.</p>
-            <ul>
-                <li>Perform the calculation</li>
-                <li>Display the result</li>
-            </ul>
-            <p>There are a number of different ways that we can execute both of these steps, but at first, we'll use the <CodeInline>print</CodeInline> command.</p>
-
-            <p>When we use the <CodeInline>print</CodeInline> command, we can place the calculation we want to perform in the brackets.  The <CodeInline>print</CodeInline> command will perform the calculation and print the result to the console.</p>
-
-            <Python code={`print(<Calculation to Perform>)`}/>
-
-            <p>For Example.  To calcuate the sum of 2 and 5 (2 plus 5) and 3 squared, we type:</p>
-                            
-            <CodeExample 
-                code={`print(2 + 5)\nprint(3 ** 2)`}
-                output={`7\n9`}
-            />
-
-            <Note>
-                <ul>
-                    <li>There is no indentation infront of the <CodeInline>print</CodeInline> command</li>
-                    <li>There are no quotes around the calculation being performed.</li>
-                </ul>
-            </Note>
-                    
+            <p>To declare a string variable, use quotes.</p>
+            
+            <Python code={`name = "John Smith"`}></Python>
+            <p>Python also allows you to declare strings using the single quote <CodeInline>'</CodeInline>.</p>
+            <Python code={`name = 'John Smith'`}/>
+            
+        
             </SectionText>
         </Section>
 
         <Section>
             <SectionTitle><CodeInline>len</CodeInline> Command</SectionTitle>
-            <SectionText></SectionText>
+            <SectionText>
+                <p>The <CodeInline>len</CodeInline> command will return the number of characters in a given string.  This can be used to test that a password contains a certain number of characters, or some other form of validation.</p>
+
+                <CodeExample code={`print("There are ", len("Salih"), "charcters in 'Salih'")`}
+                      output={`There are 5 characters in 'Salih'`}
+                ></CodeExample>
+            </SectionText>
+        </Section>
+
+        <Section>
+            <SectionTitle><CodeInline>+ (Append)</CodeInline> Operator</SectionTitle>
+            <SectionText>
+                <p>The <CodeInline>+</CodeInline> operator will return a new string that is 2 strings joined together.</p>
+
+                <CodeExample code={`print("Mr" + "Salih")`}
+                      output={`Mr Salih`}
+                ></CodeExample>
+            </SectionText>
+        </Section>
+
+        <Section>
+            <SectionTitle><CodeInline>* (Repeat)</CodeInline> Operator</SectionTitle>
+            <SectionText>
+                <p>The <CodeInline>*</CodeInline> operator will return a new string that is the first string repeated a number of times.</p>
+
+                <CodeExample code={`print('*' * 5)\nprint("ABC" * 3)`}
+                      output={`*****\nABCABCABC`}
+                ></CodeExample>
+            </SectionText>
         </Section>
         
         <Section>
             <SectionTitle>Index Operator <CodeInline>[]</CodeInline></SectionTitle>
-            <SectionText></SectionText>
+            <SectionText>
+                <p>The <CodeInline>[]</CodeInline>index operator will return a section of a string; either a single character, or a selection of characters from the string.</p>
+                <p>The first character is at index 0, and the last character is at index 4.  The index of the last character can be calculated using <CodeInline>len(string) - 1</CodeInline></p>
+                <p>Python strings can also be indexed from the end of the string using the reversal index.   This starts at -1, indicating the last character, and decreases as you move towards the start of the string.</p>
+                <p>Using the code <CodeInline>name = "Salih"</CodeInline></p>
+
+                <p>
+                    <table>
+                        <tr>
+                            <td>Index</td>
+                            <td>0</td>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>Reverse Index</td>
+                            <td>-5</td>
+                            <td>-4</td>
+                            <td>-3</td>
+                            <td>-2</td>
+                            <td>-1</td>
+                        </tr>
+                        <tr>
+                            <td>Character</td>
+                            <td>S</td>
+                            <td>a</td>
+                            <td>l</td>
+                            <td>i</td>
+                            <td>h</td>
+                        </tr>
+                    </table>
+                </p>
+                
+
+            </SectionText>
+        </Section>
+        <Section>
+            <SectionTitle>Index Ranges</SectionTitle>
+            <SectionText>
+            <p>We can also use the colon symbol with in the index operator to indicate a range of characters.</p>
+            <p>The code is written as <CodeInline>[start:end]</CodeInline> where the start is included, but the end is not included.</p>
+            <p>The following table shows several examples of the index, reverse index and range operators</p>
+            <p>Using the code <CodeInline>name = "Salih"</CodeInline></p>
+            <table>
+                <tr><td>Index operator</td><td>Result</td></tr>
+                <tr><td>name[0]</td><td>S</td></tr>
+                <tr><td>name[1]</td><td>a</td></tr>
+                <tr><td>name[2]</td><td>l</td></tr>
+                <tr><td>name[0:2]</td><td>Sa</td></tr>
+                <tr><td>name[1:4]</td><td>ali</td></tr>
+                <tr><td>name[-1]</td><td>h</td></tr>
+                <tr><td>name[-4]</td><td>a</td></tr>
+                <tr><td>name[-4:]</td><td>alih</td></tr>
+                <tr><td>name[-4:-1]</td><td>ali</td></tr>
+            </table>
+            </SectionText>
+            
         </Section>
 
-        <Activity repl="https://replit.com/@mrsalih/operations-numeric#challenge.md" 
+        <Activity repl="https://replit.com/@mrsalih/operators-strings#challenge.md" 
                           email={email} 
-                          challengeName="level-1::operations-numeric" 
+                          challengeName="level-1::operators-string" 
                           pupilProgress={pupilProgress}
                           />
 
