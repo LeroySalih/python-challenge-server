5da3a0b5a670eecbe4b5534848e8be1b48ac28e5
Leroy Salih <leroy.salih@gmail.com>
2021-10-18 16:00:16 +0000

    Removed print section from transpose

--- components/level-3/transpose.js
+++ components/level-3/transpose.js
@@ -36,51 +36,7 @@ const MarkUp = ({email,  pupilProgress}) => (<motion.div variants={ContainerVari
         </KeyTerms>
     </Section>
                 
-    <Section>
-        <SectionTitle >Transpose Cypher</SectionTitle>
-        <SectionText>
-        <p>We can use the <CodeInline>print</CodeInline> command to display numbers, text and characters to the 
-        console.  In text based programs, the console is the main interface that we use to 
-        allow our program to interact with the user. Other ways that we interact with users include sound and files, but we will deal with these in later lessons.
-        </p>
-        <p>Typical uses for the <CodeInline>print</CodeInline> command inlude:</p>
-        <ul>
-            <li>Letting the user know the result of a calculation</li>
-            <li>Giving the user an update on a long running operation</li>
-            <li>Letting the user know something has gone wrong</li>
-            <li>Letting the user know that the program has ended.</li>
-        </ul>
-        
-
-        <p>To use the <CodeInline>print</CodeInline> command, type:</p>
-
-
-        <Python code={`print(<something to output to console>)`}/>
-        
-        
-        <p>Some examples are:</p>
-        <CodeExample 
-            code={`print("Hello World")\nprint(10 + 5)`}
-            output={`Hello World\n15`}
-        />
-        
-        </SectionText>
-    </Section>
-
-    <Section >
-        <SectionTitle >Printing Numbers</SectionTitle>
-        <SectionText>
-            
-            <p>To print a number simply put the number between the brackets</p>
-            <CodeExample 
-                code={`print(10)\nprint(0.5)\nprint(0.1234567)`}
-                output={`10\n0.5\n0.1234567`}
-            />
-            
-        </SectionText>
-        
-    </Section>
-            
+    
     <Activity title="Transpose Cypher"
                 repl="https://replit.com/@mrsalih/transpose#challenge.md" 
                 email={email} 
